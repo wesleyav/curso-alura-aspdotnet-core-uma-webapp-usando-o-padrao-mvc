475474f894d76db5d2f48e9c8d13008a714923ed
Wesley <wesley.contato@hotmail.com>
2020-11-26 05:11:52 -0300

    Nossa aplicação na Web

--- Alura.ListaLeitura.App/Program.cs
+++ Alura.ListaLeitura.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
 using System;
 
 namespace Alura.ListaLeitura.App
@@ -10,9 +11,15 @@ namespace Alura.ListaLeitura.App
         {
             var _repo = new LivroRepositorioCSV();
 
-            ImprimeLista(_repo.ParaLer);
-            ImprimeLista(_repo.Lendo);
-            ImprimeLista(_repo.Lidos);
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
         }
 
         static void ImprimeLista(ListaDeLeitura lista)
