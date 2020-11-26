3afea49ddf5a11e2cb438db839ed0f276d37bce6
Wesley <wesley.contato@hotmail.com>
2020-11-26 06:04:24 -0300

    Melhorando a lógica de tratamento de requisições

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -19,16 +19,17 @@ namespace Alura.ListaLeitura.App
         public Task Roteamento(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
-            var caminhosAtendidos = new Dictionary<string, string>
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
             {
-                {"/Livros/ParaLer", _repo.ParaLer.ToString() },
-                {"/Livros/Lendo", _repo.Lendo.ToString() },
-                {"/Livros/Lidos", _repo.Lidos.ToString() }
+                {"/Livros/ParaLer", LivrosParaLer },
+                {"/Livros/Lendo", LivrosLendo },
+                {"/Livros/Lidos", LivrosLidos }
             };
 
             if (caminhosAtendidos.ContainsKey(context.Request.Path))
             {
-                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
             }
 
             context.Response.StatusCode = 404;
@@ -37,12 +38,20 @@ namespace Alura.ListaLeitura.App
 
         public Task LivrosParaLer(HttpContext context)
         {
-
             var _repo = new LivroRepositorioCSV();
-
             return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
 
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
 
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }
