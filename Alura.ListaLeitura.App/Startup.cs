bccc5097cdd0a2bffc8ac0701873a9943ceefeee
Wesley <wesley.contato@hotmail.com>
2020-11-26 05:23:38 -0300

    Uma resposta padrão

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,9 +1,27 @@
-﻿namespace Alura.ListaLeitura.App
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        public void Configure()
+        public void Configure(IApplicationBuilder app)
         {
+            app.Run(LivrosParaLer);
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+
+            var _repo = new LivroRepositorioCSV();
+
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+
 
         }
     }
